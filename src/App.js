f0cbf01a975af2ba6d6e032cd7a6a24bbc7e22bb
jallend1 <jallend1@gmail.com>
2021-03-01 17:55:47 -0800

    Integrates navbar styling into main CSS file; Incorporates React Router; Adds error component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import {Switch, Route} from 'react-router-dom';
+
+import NavBar from './Components/NavBar'
+
 import Header from "./Components/Header";
-import NavBar from "./Components/NavBar";
 import Sidebar from "./Components/Sidebar";
 import NextActions from "./Components/NextActions";
+import About from './Components/About';
+import Error from './Components/Error';
 import Footer from "./Components/Footer";
 
 function App() {
@@ -9,8 +14,18 @@ function App() {
     <div className="App">
       <NavBar />
       <Header />
-      <NextActions />
-      <Sidebar />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route exact path="/">
+          <NextActions />
+          <Sidebar />
+        </Route>
+        <Route path="/">
+          <Error />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
