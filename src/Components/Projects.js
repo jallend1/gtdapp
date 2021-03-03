514f91491010047ed863f7c8bdf1e5509b508021
jallend1 <jallend1@gmail.com>
2021-03-02 21:41:53 -0800

    Structures out basic Project list page using the dummy data

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,27 +1,45 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const Projects = () => {
-    const [projects, setProjects] = useState([]);
-    
-    const getProjects = () => {
-        fetch('data.json')
-            .then(res => res.json())
-            .then(data => setProjects(data));
-    }
+  const [projects, setProjects] = useState([]);
 
+  const getProjects = () => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setProjects(data));
+  };
 
-    // useEffect(() => getProjects(), []);
-    return (
-        <div className="container">
-            <header>
-                <h2>Project Page!</h2>            
-            </header>
-            <main>
-
-            </main>
-
+  const renderProjects = () => {
+    return projects.map((project) => {
+      const createdAt = new Date();
+      return (
+        <div className="project" key={project.id}>
+          <div className="project-head">
+            <h3>{project.title}</h3>
+            <div>Star Icon Goes Here</div>
+          </div>
+          <div className="project-body">
+            <p>{project.body}</p>
+          </div>
+          <div className="project-footer">
+            <p>Created at: {createdAt.toDateString()}</p>
+            <p>Posted by userID: {project.userId}</p>
+          </div>
         </div>
-    )
-}
+      );
+    });
+  };
 
-export default Projects
+  useEffect(() => getProjects(), []);
+
+  return (
+    <div className="container">
+      <header>
+        <h2>Project List</h2>
+      </header>
+      <main>{renderProjects()}</main>
+    </div>
+  );
+};
+
+export default Projects;
