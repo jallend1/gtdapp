770a8567b450862d30ffa0314506391ca83510f2
jallend1 <jallend1@gmail.com>
2021-03-04 16:33:49 -0800

    Adds starred field to data file and beings work on creating new project form

--- src/Components/NewProject.js
+++ src/Components/NewProject.js
@@ -1,26 +1,21 @@
 const NewProject = () => {
   return (
-    <div>
+    <div className="new-project">
       <header>
         <h2>New Project</h2>
       </header>
       <section>
         <form>
-          <label htmlFor="title">Project Name</label>
-          <input
-            type="text"
-            name="title"
-            id="title"
-            placeholder="Name your project"
-          ></input>
-          <label htmlFor="user">User ID</label>
-          <input
-            type="text"
-            name="user"
-            id="user"
-            placeholder="Your user number"
-          ></input>
-          <label htmlFor="nextAction1">Next Action:</label>
+          <div>
+            <label htmlFor="title">Project Name</label>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              placeholder="Name your project"
+            ></input>
+          </div>
+          <label htmlFor="nextAction1">Add a next action:</label>
           <input
             type="text"
             name="nextAction1"
