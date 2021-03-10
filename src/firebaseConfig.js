10653ef36cbf730b757994fff357d1078fc038dc
jallend1 <jallend1@gmail.com>
2021-03-10 15:36:31 -0800

    Updates GitIgnore file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,7 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-// Place Firebase Config data for the project here
+var firebaseConfig = {
+    apiKey: "AIzaSyAYEkFdyXOR_qrXH2_JBhhKDKPu9n4Rc5g",
+    authDomain: "gtdapp-6e9d0.firebaseapp.com",
+    projectId: "gtdapp-6e9d0",
+    storageBucket: "gtdapp-6e9d0.appspot.com",
+    messagingSenderId: "545393890835",
+    appId: "1:545393890835:web:b76d9ef8797f0751dcd1e6"
+  };
 
 firebase.initializeApp(firebaseConfig);
 firebase.firestore().settings({ timestampsinSnapshots: true});
