d2518585c7fdc16e2a9796e847d26085c6d5a3d1
jallend1 <jallend1@gmail.com>
2021-04-03 14:59:38 -0700

    Implements ability to rerender NextActions list on the front page by age of project

--- src/Components/Projects/NextActions.js
+++ src/Components/Projects/NextActions.js
@@ -1,31 +1,65 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import RenderAction from "./RenderAction";
 import { ProjectContext } from "../../Contexts/ProjectContext";
 
 const NextActions = () => {
   const { projects } = useContext(ProjectContext);
-  const determineNextAction = (project) => {
-    const actionList = project.nextActions.filter(
-      (action) => action.isComplete === false
-    );
-    if (actionList[0]) {
-      return (
-        <RenderAction
-          action={actionList[0]}
-          project={project}
-          key={project.id + actionList[0].step}
-          needsURL={true}
-        />
+  const [sortDate, setSortDate] = useState(true);
+  const [nextActionList, setNextActionList] = useState([])
+  
+  const determineNextAction = () => {
+    const newActionList = [];
+    projects.forEach(project => { 
+      const actionList = project.nextActions.filter(
+        (action) => action.isComplete === false
       );
-    } else {
-      return null;
-    }
+      let nextAction;
+      if(actionList[0]){
+        nextAction = {
+          action: actionList[0],
+          project: project,
+          key: project.id + actionList[0].step,
+          needsURL: true,
+          created: project.createdAt
+        }
+        newActionList.push(nextAction)
+      }
+      else {
+        return null;
+      }
+    });
+    sortDate ? newActionList.sort((a, b) => a.created < b.created ? -1 : 1) : newActionList.sort((a,b) => a.created > b.created ? -1 : 1);
+    setNextActionList(newActionList);
   };
+
+  const renderProjects = () => {
+    return nextActionList.map(nextAction => {
+      return <RenderAction
+      action={nextAction.action}
+      project={nextAction.project}
+      key={nextAction.key}
+      needsURL={nextAction.needsURL}
+    />
+    })
+  }
+
+  const toggleSort = () => {
+    setSortDate(!sortDate);
+  }
+
+  
+  useEffect(determineNextAction, [projects, sortDate])
+
   return (
     <div>
       <h2>Next Actions</h2>
+      <div onClick={toggleSort}>
+        <span className="material-icons">swap_vert</span>
+        Sort <span>{sortDate ? 'newest' : 'oldest'} </span> projects first
+
+      </div>
       <ul>
-        {projects && projects.map((project) => determineNextAction(project))}
+        {projects && renderProjects()}
       </ul>
     </div>
   );
