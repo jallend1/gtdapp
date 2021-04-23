19fef437cdbeab1122e504b32d3f020fe5f50b41
jallend1 <jallend1@gmail.com>
2021-04-23 16:20:04 -0700

    Redirects to home page if last action on NEW project page is blank. Ignores it if on existing project page

--- src/Components/Projects/AddActionForm.js
+++ src/Components/Projects/AddActionForm.js
@@ -12,10 +12,16 @@ const AddActionForm = ({ projectId }) => {
 
   const handleSubmit = (e) => {
     const task = e.target.nextAction.value;
-    // If the next action input is blank when submitted, navigates away from current page back home
-    if (task.trim().length === 0) {
+    // If the next action input is blank when submitted AND is on the NEW project page (History does not exist on individual project page), navigates away from current page back home
+    if (task.trim().length === 0 && history.location.pathName) {
       history.push("/");
-    } else {
+    } 
+    // Otherwise, if the new task is blank on an existing project page, just ignores it
+    else if(task.trim().length === 0){
+      e.preventDefault();
+      return;
+    }
+    else {
       addAction(e, projectId);
       setNextAction("");
     }
