807009ee446a17abf8f52eb6c4fca24fdb36e988
jallend1 <jallend1@gmail.com>
2021-04-28 15:10:03 -0700

    Continues work on making message display disappear

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -3,10 +3,25 @@ import { ProjectContext } from "../Contexts/ProjectContext";
 
 const Message = () => {
   const { messageDetails, updateMessage } = useContext(ProjectContext);
+  
+  const fadeMessage = () => {
+    // TODO: Rather than messing with all this stuff (which very much does not work), maybe just set up the message itself to an empty string?
+    const $message = document.getElementById('message');
+    $message.classList.remove('message-hide');
+    $message.classList.add('message-fade');  
+    setTimeout(() => {
+      $message.classList.add('message-hide');
+      $message.classList.remove('message-fade');
+      console.log('changing it!')
+      console.log($message.classList)
+    }, 3000)
+    console.log($message.classList)
+  }    
   // useEffect(() => setTimeout(updateMessage({type: 'none'}), 1500), [messageDetails])
+  useEffect(() => fadeMessage(), [messageDetails]);
 
   return (
-    <div className={`message message-${messageDetails.type}`}>
+    <div className={`message message-${messageDetails.type} message-hide`} id="message">
       {messageDetails.message}
     </div>
   );
